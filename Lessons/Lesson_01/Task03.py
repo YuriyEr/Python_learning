7d09e8387485c044de925cf5ff36844b284359fc
Юрий <y.ershov@bk.ru>
2020-07-07 19:24:40 +0300

    Lesson01-04

--- Lessons/Lesson_01/Task03.py
+++ Lessons/Lesson_01/Task03.py
@@ -5,15 +5,32 @@ import random
 import time
 
 num = 0
-print("Загадайте число от 0 до 20")
-time.sleep(3)
+print("Загадайте число от 0 до 20!")
+time.sleep(1)
 print("Загадали?")
 time.sleep(1)
-print("Ваше число меньше 20? Да или Нет?")
-inputValueFirst = input("Ваше число меньше 20? Да или Нет?")
-while inputValueFirst != "Да" | inputValueFirst != "Нет":
-    inputValueFirst = input("Я вас не поняла, это число меньше 20? Да или Нет?")
+inputValueFirst = input("Ваше число меньше 10? Да или Нет? >> ").lower()
+while inputValueFirst != "да" and inputValueFirst != "нет" :
+    inputValueFirst = input("Я вас не поняла, это число меньше 10? Да или Нет? >> ")
 
-inputValueSecond = input("Ваше число меньше 10? Да или Нет?")
-while inputValueSecond != "Да" | inputValueSecond != "Нет":
-    inputValueSecond = input("Я вас не поняла, это число меньше 10? Да или Нет?")
+if inputValueFirst == "да":
+    inputValueSecond = input("Ваше число {}? Да? >> ".format(num)).lower()
+    while inputValueSecond != "да":
+        num += 1
+        inputValueSecond = input("Ваше число {}? Да? >> ".format(num))
+        if num > 20:
+            print("Вы загадали число не из диапазона. Досвидание.")
+            break
+    print("Отлично")
+
+else:
+    num = 10
+    inputValueSecond = input("Ваше число {}? Да? >> ".format(num)).lower()
+    while inputValueSecond != "да":
+        num += 1
+        inputValueSecond = input("Ваше число {}? Да? >> ".format(num))
+        if num > 20:
+            print("Вы загадали число не из диапазона. Досвидание.")
+            break
+
+    print("Отлично")
