ca38839de59e01158a4dbe43cbb51c272ffb4293
Юрий <y.ershov@bk.ru>
2020-07-12 12:54:25 +0300

    lesson_02-04

--- Lessons/Lesson_02/Task_03.py
+++ Lessons/Lesson_02/Task_03.py
@@ -1,3 +1,26 @@
 # 3. Пользователь вводит месяц в виде целого числа от 1 до 12.
 # Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
 # Напишите решения через list и через dict.
+
+import random
+import time
+
+enum = list(range(1,13))
+name = ("Зима", "Весна", "Лето", "Осень")
+someDict = {}
+for value in enum:
+    if  value == 12 or 1 <= value <= 2:
+        someDict[value] = name[0]
+    if 3 <= value <= 5:
+        someDict[value] = name[1]
+    if 6 <= value <= 8:
+        someDict[value] = name[2]
+    if 9 <= value <= 11:
+        someDict[value] = name[3]
+
+# input print emulate
+for value in range(1,13):
+    randomInput = random.randint(1, 12)
+    print(f"Вы ввели значение {randomInput} и это соответствует периоду {someDict.get(value)}")
+    time.sleep(0.5)
+
--- Lessons/Lesson_02/Task_04.py
+++ Lessons/Lesson_02/Task_04.py
@@ -2,3 +2,40 @@
 # Вывести каждое слово с новой строки.
 # Строки необходимо пронумеровать.
 # Если в слово длинное, выводить только первые 10 букв в слове.
+
+# someInput = input("Напишите любое предложение\n")
+
+# someInput = "Маша любит Сашу и СамантуРитуДжонс"
+someInput = input("Введите произворльное предложение \n")
+
+someList = []
+buffer = ""
+
+for value in someInput:
+    if value != " ":
+        buffer = buffer + value
+    else:
+        if len(buffer) > 10:
+            newBuffer = ""
+            for value in range(0,10):
+                newBuffer = newBuffer + buffer[value]
+                someList.append(newBuffer)
+                buffer = ""
+        else:
+            someList.append(buffer)
+            buffer = ""
+
+if buffer != " " and buffer != "":
+    if len(buffer) > 10:
+        newBuffer = ""
+        for value in range(0, 10):
+            newBuffer = newBuffer + buffer[value]
+        someList.append(newBuffer)
+        buffer = ""
+    else:
+        someList.append(buffer)
+        buffer = ""
+
+print("Результат упражнения")
+for key, result in enumerate(someList):
+    print(f"{key+1} {result}")
