a3b8100760e204e37927fb02190ed3b7788ad32e
Юрий <y.ershov@bk.ru>
2020-07-24 16:22:05 +0300

    Task_05-05

--- Lessons/Lesson_05/Task_04.py
+++ Lessons/Lesson_05/Task_04.py
@@ -2,3 +2,29 @@
 4. Создать (программно) текстовый файл, записать в него программно набор чисел,
 разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+
+import os
+import random
+
+nameObj = r"Task04-01 Text.txt"
+
+if os.path.exists(nameObj):
+    os.remove(nameObj)
+try:
+    with open(nameObj, "w+") as obj:
+        data = [random.randint(0,20) for x in range(100, 150)]
+        result: str = ""
+        for value in data:
+            result = result + " " + str(value)
+        obj.write(result)
+        print(f"Результат генерации: \n{result}\n")
+except:
+    print("Ошибка в блоке генерации и записи")
+
+try:
+    with open(nameObj) as obj:
+        metaData = obj.read().split()
+        metaData = list((map(lambda x: int(x), metaData)))
+        print(f"Сумма всех сгенерированных чисел из файла:\n{sum(metaData)}")
+except:
+    print("Ошибка в блоке десериализации и подсчета")
--- Lessons/Lesson_05/Task_05.py
+++ Lessons/Lesson_05/Task_05.py
@@ -1,8 +1,10 @@
 """
-5. Необходимо создать (не программно) текстовый файл,
+5.
+A Необходимо создать (не программно) текстовый файл,
 где каждая строка описывает учебный предмет и наличие лекционных,
 практических и лабораторных занятий по этому предмету и их количество.
 Важно, чтобы для каждого предмета не обязательно были все типы занятий.
+B
 Сформировать словарь, содержащий название предмета и общее количество занятий по нему.
 Вывести словарь на экран.
 
@@ -11,3 +13,44 @@
 Физкультура: — 30(пр) —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+
+import os
+import re
+import numpy
+
+nameObj = r"Task05-01 Text.txt"
+# A
+if os.path.exists(nameObj):
+    os.remove(nameObj)
+try:
+    with open(nameObj, "a") as obj:
+        data = [["Информатика", {"лекций": 100, "пар": 50, "лабораторных": 20 }],
+                ["Физика", {"лекций": 30, "лабораторных": 10 }],
+                ["Физкультура", {"пар": 30}],
+                ["Выживание", {"лекций": 80, "лабораторных": 30}],
+                ]
+        for val in data:
+            stringValue = ""
+            stringValue = f"{val[0]}: "
+            for key, value in val[1].items():
+                stringValue += f"{value}"
+                stringValue += f"({key}) "
+            stringValue += "\n"
+            obj.write(stringValue)
+except:
+    print("Ошибка в блоке генерации и записи в файл")
+
+# B
+with open(nameObj) as obj:
+    unpackData = dict()
+    for val in obj.readlines():
+        key = val.split()[0].replace(":", "")
+        values = []
+        for vals in val.split():
+            buffer = (re.findall('\d+', vals))
+            if buffer != []:
+                values.append(int(buffer[0]))
+        unpackData[key] = sum(values)
+    print(f"результат задания распаковки\n{unpackData}")
+
+
